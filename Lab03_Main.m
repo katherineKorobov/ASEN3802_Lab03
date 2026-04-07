5cc0dd4d59fe92b07ef9f66a1becd1dcb7cd7e1b
JohnHeflinCU <johe7835@colorado.edu>
2026-04-07 12:33:46 -0600

    update

--- Lab03_Main.m
+++ Lab03_Main.m
@@ -12,14 +12,6 @@ close all
 
 %% Part 1
 
-
-
-
-%%Task 1
-
-c=1; % chord length (m)
-N=50; % number of panels
-
 %% Task 1
 c=1; % chord length (m)
 N=50; % number of panels
@@ -48,6 +40,7 @@ t_2 = 0.21 * c;
 
 [x_b2, y_b2] = NACA_airfoils(m_2, p_2, t_2, c, N); 
 y_c=y_b2(1:N)+y_b2(N+1:2.*N);
+
 figure
 hold on; 
 plot(x_b2(1:N), y_b2(1:N),'b') % upper surface
