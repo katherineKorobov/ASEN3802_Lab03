06cde14b89fdcc9c3cff507f42c49f9525d2e692
Katherine Korobov <katya.korobov@gmail.com>
2026-04-07 13:02:51 -0600

    starting function

---
+++ calculateZeroLiftAlpha.m
@@ -0,0 +1,15 @@
+function zero_lift_alpha = calculateZeroLiftAlpha(x, c)
+% Finds the zero lift angle of attack given x
+% Detailed explanation goes here
+%
+% Author: {primary author, should be you}
+% Collaborators: J. Doe, J. Smith {acknowledge whomever you worked with}
+% Date: {should include the date last revised}
+
+
+
+integrand = @(camber_slope,)
+
+zero_lift_alpha = -1/pi ()
+
+end
