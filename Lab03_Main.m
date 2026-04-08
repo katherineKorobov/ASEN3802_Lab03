59678e0f2ccc5f109bece4aa27ca6ed56b2f4826
Katherine Korobov <katya.korobov@gmail.com>
2026-04-08 11:35:04 -0600

    working on task 3

--- Lab03_Main.m
+++ Lab03_Main.m
@@ -1,6 +1,4 @@
-clc
-clear
-close all
+clc; clear; close all;
 %% ASEN 3802 - Lab 03 - Main
 % Part 1: Analysis of 2D Airfoils
 %   Task 1: NACA 4-digit Airfoil Generator
@@ -11,17 +9,14 @@ close all
 
 
 %% Part 1
-
 %% Task 1
 c=1; % chord length (m)
 N=50; % number of panels
 
 %NACA 0021 ----------------
-m = 0;
-p = 0;
-t = 0.21 .* c; 
+param_0012 = struct("m", 0, "p", 0, "t", 0.12 * c);
 
-[x_b, y_b] = NACA_airfoils(m, p, t, c, N); 
+[x_b, y_b] = NACA_airfoils(param_0012.m, param_0012.p, param_0012.t, c, N); 
 figure
 hold on; 
 plot(x_b(1:N), y_b(1:N),'b') % upper surface
@@ -34,11 +29,9 @@ title("NACA 0021")
 hold off;
 
 %NACA 2421 ----------------
-m_2 = 0.02 * c;
-p_2 = 0.40 * c; 
-t_2 = 0.21 * c; 
+param_2421 = struct("m",0.02 * c, "p", 0.40 * c ,"t", 0.21 * c); 
 
-[x_b2, y_b2] = NACA_airfoils(m_2, p_2, t_2, c, N); 
+[x_b2, y_b2] = NACA_airfoils(param_2421.m, param_2421.p, param_2421.t, c, N); 
 y_c=y_b2(1:N)+y_b2(N+1:2.*N);
 
 figure
@@ -57,14 +50,11 @@ hold off;
 
 %% task 2
 % NACA 0012
-m = 0; 
-p = 0;
-t = 12/100;
 alpha = 12; % degrees
 v_inf = 50;
 c_l_actual = 1.25;
 N = 2;
-[x_b,y_b] = NACA_airfoils(m,p,t,c,N);
+[x_b,y_b] = NACA_airfoils(param_0012.m, param_0012.p, param_0012.t,c,N);
 
 if mod(N,2)==0
     xb = [flip(x_b(N+1:2*N)); x_b(2:N-1,1)];
@@ -79,7 +69,7 @@ error(N) = (CL(N) - c_l_actual)/c_l_actual;
 
 while abs(error(N)) > 0.01
     N = N + 1;
-    [x_b,y_b] = NACA_airfoils(m,p,t,c,N);
+    [x_b,y_b] = NACA_airfoils(param_0012.m, param_0012.p, param_0012.t,c,N);
     if mod(N,2)==0
         xb = [flip(x_b(N+1:2*N)); x_b(2:N-1,1)];
         yb = [flip(y_b(N+1:2*N)); y_b(2:N-1,1)];
@@ -102,11 +92,11 @@ title('Convergence of the predicted sectional coefficient of lift (c_l) with res
 
 %% Task 3
 
-param_0012 = struct("m", 0, "p", 0, "t", 12/100);
-param_0006 = struct("m", 0, "p", 0, "t", 6/100);
-param_0018 = struct("m", 0, "p", 0, "t", 18/100);
+% * NACA 0012 is defined above earlier
+param_0006 = struct("m", 0, "p", 0, "t", 0.06 * c);
+param_0018 = struct("m", 0, "p", 0, "t", 0.18 * c);
 
-num_panels = 50; % will change after task 2 finished
+num_panels = 30; % will change after task 2 finished
 
 % build airfoils
 [x_0006,y_0006] = NACA_airfoils(param_0006.m, param_0006.p, param_0006.t, c, num_panels);
@@ -115,9 +105,9 @@ num_panels = 50; % will change after task 2 finished
 
 % we need to flip the x and y to go from trailing edge to leading edge
 % clockwise
-[flipped_x_0006, fipped_y_0006] = flipPositions(x_0006, y_0006);
-[flipped_x_0012, fipped_y_0012] = flipPositions(x_0012, y_0012);
-[flipped_x_0018, fipped_y_0018] = flipPositions(x_0018, y_0018);
+[flipped_x_0006, flipped_y_0006] = flipPositions(x_0006, y_0006);
+[flipped_x_0012, flipped_y_0012] = flipPositions(x_0012, y_0012);
+[flipped_x_0018, flipped_y_0018] = flipPositions(x_0018, y_0018);
 
 % collect experimental data
 data_0006 = load("NACA_0006_data.mat");
@@ -127,13 +117,13 @@ experimental_0006 = data_0006.data;
 experimental_0012 = data_0012.data;
 
 % create range for alpha
-alpha = linspace(-10, 10, 20); % AoA from 0 deg to 45 deg
+alpha = linspace(-10, 10, 20); % [deg]
 
 for i = 1:length(alpha)
       
-    cl_0006(i) = Vortex_Panel(x_0006, y_0006, 1, alpha(i));
-    cl_0012(i) = Vortex_Panel(x_0012, y_0012, 1, alpha(i));
-    cl_0018(i) = Vortex_Panel(x_0018, y_0018, 1, alpha(i));
+    cl_0006(i) = Vortex_Panel(flipped_x_0006, flipped_y_0006, 1, alpha(i));
+    cl_0012(i) = Vortex_Panel(flipped_x_0012,flipped_y_0012, 1, alpha(i));
+    cl_0018(i) = Vortex_Panel(flipped_x_0018, flipped_y_0018, 1, alpha(i));
 
 end
 
@@ -145,4 +135,26 @@ plot(alpha, cl_0018);
 plot(experimental_0006(:, 1), experimental_0006(:, 2));
 plot(experimental_0012(:, 1), experimental_0012(:, 2));
 hold off;
-legend("Predicted NACA 0006", "Predicted NACA 0012", "Predicted NACA");
+legend("Predicted NACA 0006", "Predicted NACA 0012", "Predicted NACA 0018", ...
+    "Experimental NACA 0006", "Experimental NACA 0012", "Location","southeast");
+xlim([-11, 11]);
+xlabel("Angle of Attack [deg]");
+ylabel("Sectional Coefficient of Lift");
+
+% estimation of zero lift angle of attack 
+zero_lift_aoa_0006 = interp1(cl_0006, alpha, 0, "linear");
+zero_lift_aoa_0012 = interp1(cl_0012, alpha, 0, "linear");
+zero_lift_0018 = interp1(cl_0018, alpha, 0, "linear");
+zero_lift_aoa_exp_0006 = interp1(experimental_0006(:, 2), experimental_0006(:,1), 0, "linear");
+zero_lift_aoa_exp_0012 = interp1( experimental_0012(:, 2), experimental_0012(:,1), 0, "linear");
+
+% estimatoin of lift slope 
+
+lift_slope_0006 = calculateLiftSlope(alpha, cl_0006);
+lift_slope_0012 = calculateLiftSlope(alpha, cl_0012);
+lift_slope_0018 = calculateLiftSlope(alpha, cl_0018);
+lift_slope_exp_0006 = calculateLiftSlope(experimental_0006(:, 1), experimental_0006(:, 2));
+lift_slope_exp_0012 = calculateLiftSlope(experimental_0012(:, 1), experimental_0012(:, 2));
+
+
+
--- flipPositions.m
+++ flipPositions.m
@@ -3,16 +3,15 @@ function [x_flipped,y_flipped] = flipPositions(x,y)
 % Vortext panel method requires the x and y location sto be arranged in a 
 % clockwise order starting from teh trailing edge
 
-trailing_edge_idx = length(x)/2;
+N = length(x)/2;
 
-x_half = x(1:trailing_edge_idx); % go from beginning ot trailing edge 0 -> 1
-y_half = y(1:trailing_edge_idx); % corresponding y values up to the trailing edge
-
-x_flipped = flip(x_half); % flip the x positions
-y_flipped = flip(y_half); % flip the corresponding y positions
-
-x_flipped = [-x_flipped, x_half];
-y_flipped = [-y_flipped, y_half];
+if mod(N,2) == 0
+    x_flipped = [flip(x(N+1:2*N)); x(2:N-1,1)];
+    y_flipped = [flip(y(N+1:2*N)); y(2:N-1,1)];
+else
+    x_flipped = [flip(x(N+2:2*N)); x(1:N-1,1)];
+    y_flipped = [flip(y(N+2:2*N)); y(1:N-1,1)];
+end
 
 
 
