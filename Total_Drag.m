ce22ae5dfb1666b0917d1902ece860afe97871d4
Kiana <111478830+K1anaW@users.noreply.github.com>
2026-04-21 12:31:45 -0600

    Update Total_Drag.m

--- Total_Drag.m
+++ Total_Drag.m
@@ -1,7 +1,16 @@
 function C_D=Total_Drag(C_di,c_d)
 %Inputs: sectional drag coef., induced drag coef.
 %outputs: Total drag coef
-%Author: John Heflin
-C_D=C_di+c_d
+%Author: John Heflin and Kiana Watson
+
+mat = load('NACA_0012_cd.mat');
+cd_0012 = mat.sorted_data; 
+
+mat = load('NACA_2412_cd.mat');
+cd_2412 = mat.sorted_data; 
+
+C_Dm= C_di + c_d
+
+
 
 end
